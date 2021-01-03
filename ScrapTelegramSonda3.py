6d1013ef923a5d888fa9be1f7eecc184928a22ff
Ellena, Ezequiel <eellena@bcr.com.ar>
2021-01-03 20:50:04 -0300

    actualizacion select

--- ScrapTelegramSonda3.py
+++ ScrapTelegramSonda3.py
@@ -54,31 +54,31 @@ def enviar_noticias(arr,id_chat,Nombre_Grupo,provincias,tema):
             linkPortal = m[1]
             linkNoticia = m[0]
             mycursor = mydb.cursor()
-            sql = "SELECT * FROM noticias_enviadas WHERE link = '"+str(linkNoticia)+"'" +""+""
-            mycursor.execute(sql)
-            records = cursor.fetchall()
-            if  records == []:
-                extracted = tldextract.extract(linkPortal)
-                medio = "{}.{}".format(extracted.domain, extracted.suffix)
-                medio = medio.replace(".com", "").replace(".ar", "")
-                linktitulo = requests.get(m[0]).text
-                titulo = funcion_BuscaTitulo(linktitulo)
-                if isinstance(titulo, str):
+            #sql = "SELECT * FROM noticias_enviadas WHERE link = '"+str(linkNoticia)+"'" +""+""
+            #mycursor.execute(sql)
+            #records = cursor.fetchall()
+            #if  records == []:
+            extracted = tldextract.extract(linkPortal)
+            medio = "{}.{}".format(extracted.domain, extracted.suffix)
+            medio = medio.replace(".com", "").replace(".ar", "")
+            linktitulo = requests.get(m[0]).text
+            titulo = funcion_BuscaTitulo(linktitulo)
+            if isinstance(titulo, str):
 
-                    mensaje = "Medio: "+medio+"\n\n"+"    Última Noticia: " + titulo + "\n\n" + "    Ver más en ->" + linkNoticia
-                    requests.post('https://api.telegram.org/' + url_api,
-                                  data={'chat_id': id_chat, 'text': mensaje})
-                    print(requests.status_codes)
-                    try:
-                        mycursor = mydb.cursor()
-                        sql = "INSERT INTO noticias_enviadas (link,tema) " \
-                              "VALUES (%s, %s) "
-                        val = (linkNoticia, temas)
-                        mycursor.execute(sql, val)
-                        mydb.commit()
-                        print("insertó correctamente el link: " + linkNoticia + "")
-                    except Exception as e:
-                        print("El Link ya fue guardado: " + linkNoticia + "")
+                mensaje = "Medio: "+medio+"\n\n"+"    Última Noticia: " + titulo + "\n\n" + "    Ver más en ->" + linkNoticia
+                requests.post('https://api.telegram.org/' + url_api,
+                              data={'chat_id': id_chat, 'text': mensaje})
+                print(requests.status_codes)
+                try:
+                    mycursor = mydb.cursor()
+                    sql = "INSERT INTO noticias_enviadas (link,tema,id_grupo) " \
+                          "VALUES (%s, %s, %s) "
+                    val = (linkNoticia, temas, id_chat)
+                    mycursor.execute(sql, val)
+                    mydb.commit()
+                    print("insertó correctamente el link: " + linkNoticia + "")
+                except Exception as e:
+                    print("El Link ya fue guardado: " + linkNoticia + "")
 
     except Exception as e:
         print(" 279 - enviar ", e)
@@ -125,7 +125,7 @@ if __name__ == '__main__':
                 Provincias = []
                 Temas = []
                 resultado = []
-                CambioGrupo = requests.get("http://167.86.120.98:6061/GrupoCanal").json()
+                #CambioGrupo = requests.get("http://167.86.120.98:6061/GrupoCanal").json()
                 ID_GRUPO = Grupo["id_grupo"]
 
                 NombreDelGrupo = Grupo["nombre_grupo"]
@@ -133,8 +133,9 @@ if __name__ == '__main__':
                     Provincias.append(pronvicia["id_provincia"])
                 prov = ','.join(str(e) for e in Provincias)
                 for tema in Grupo["temas"]:
-                    sql_select_Query = "SELECT link, medio, texto FROM todas_las_noticias WHERE texto like '%"+ tema["descripcion"] \
-                                       +"%' and provincia in ("+ prov +")"
+                    sql_select_Query = "SELECT link, medio, texto FROM todas_las_noticias WHERE texto like '%"+ tema["descripcion"]+"%' and provincia in ("+ prov +") and link not in (select link from noticias_enviadas) and link not in (select link from noticias_basura)"
+                    #sql_select_Query = "SELECT link, medio, texto FROM todas_las_noticias WHERE texto like '%"+ tema["descripcion"] \
+                    # +"%' and provincia in ("+ prov +")"
                     cursor = mydb.cursor()
                     cursor.execute(sql_select_Query)
                     #records = cursor.fetchall()
