1254cdcaa1e84030bcc8ed2e8d2a312f22c260f0
Ellena, Ezequiel <eellena@bcr.com.ar>
2021-03-04 13:48:41 -0300

    agrego try

--- ScrapRssTelegram.py
+++ ScrapRssTelegram.py
@@ -48,42 +48,46 @@ def enviar_noticias(resultado,id_chat,Nombre_Grupo,provincias,tema):
 
 if __name__ == '__main__':
 
-    while True:
         try:
-            terminacion_id = sys.argv[1]
-            GruposDeTelegram = requests.get("http://stg.kernelinformatica.com.ar:6060/GrupoCanal").json()
+            while True:
+                try:
+                    terminacion_id = sys.argv[1]
+                    GruposDeTelegram = requests.get("http://stg.kernelinformatica.com.ar:6060/GrupoCanal").json()
 
-            for Grupo in GruposDeTelegram["data"]:
-                Provincias = []
-                Temas = []
-                resultado = []
-                if Grupo["id_grupo"][-2:] == terminacion_id:
-                    ID_GRUPO = Grupo["id_grupo"]
+                    for Grupo in GruposDeTelegram["data"]:
+                        Provincias = []
+                        Temas = []
+                        resultado = []
+                        if Grupo["id_grupo"][-2:] == terminacion_id:
+                            ID_GRUPO = Grupo["id_grupo"]
 
-                    NombreDelGrupo = Grupo["nombre_grupo"]
-                    for pronvicia in Grupo["provincias"]:
-                        Provincias.append(pronvicia["id_provincia"])
-                    prov = ','.join(str(e) for e in Provincias)
-                    for tema in Grupo["temas"]:
-                        try:
-                            sql_select_Query = "SELECT link, titulo, description, content FROM todas_las_noticias_rss " \
-                                               "WHERE (titulo like "+'"%'+ tema["descripcion"]+'%"'+" or  description like "+'"%'+ tema["descripcion"]+'%"'+" or content like "+'"%'+ tema["descripcion"]+'%"'+"  )" \
-                                            "and id_provincia in ("+ prov +") " \
-                                            "and link not in (select link from noticias_enviadas  WHERE id_grupo = "+'"'+ ID_GRUPO+'"'+" " \
-                                            "and tema = "+'"'+tema["descripcion"]+'"'+")"
-                            #sql_select_Query = "SELECT link, medio, texto FROM todas_las_noticias WHERE texto like '%"+ tema["descripcion"] \
-                            # +"%' and provincia in ("+ prov +")"
-                            cursor = mydb.cursor()
-                            cursor.execute(sql_select_Query)
-                            records = cursor.fetchall()
-                            resultado.extend(records)
-                        except Exception as e:
-                            print("error mysql: " + str(e))
-                        Temas = tema["descripcion"]
-                        if not resultado == []:
-                            enviar_noticias(resultado, ID_GRUPO, NombreDelGrupo, prov, Temas)
+                            NombreDelGrupo = Grupo["nombre_grupo"]
+                            for pronvicia in Grupo["provincias"]:
+                                Provincias.append(pronvicia["id_provincia"])
+                            prov = ','.join(str(e) for e in Provincias)
+                            for tema in Grupo["temas"]:
+                                try:
+                                    sql_select_Query = "SELECT link, titulo, description, content FROM todas_las_noticias_rss " \
+                                                       "WHERE (titulo like "+'"%'+ tema["descripcion"]+'%"'+" or  description like "+'"%'+ tema["descripcion"]+'%"'+" or content like "+'"%'+ tema["descripcion"]+'%"'+"  )" \
+                                                    "and id_provincia in ("+ prov +") " \
+                                                    "and link not in (select link from noticias_enviadas  WHERE id_grupo = "+'"'+ ID_GRUPO+'"'+" " \
+                                                    "and tema = "+'"'+tema["descripcion"]+'"'+")"
+                                    #sql_select_Query = "SELECT link, medio, texto FROM todas_las_noticias WHERE texto like '%"+ tema["descripcion"] \
+                                    # +"%' and provincia in ("+ prov +")"
+                                    cursor = mydb.cursor()
+                                    cursor.execute(sql_select_Query)
+                                    records = cursor.fetchall()
+                                    resultado.extend(records)
+                                except Exception as e:
+                                    print("error mysql: " + str(e))
+                                Temas = tema["descripcion"]
+                                if not resultado == []:
+                                    enviar_noticias(resultado, ID_GRUPO, NombreDelGrupo, prov, Temas)
+                except Exception as e:
+                    print("error: " + str(e))
+                    time.sleep(2)
+                    continue
         except Exception as e:
-            print("error: " + str(e))
+            print("error ultimo: " + str(e))
             time.sleep(2)
-            continue
 
